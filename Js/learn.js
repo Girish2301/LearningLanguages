6b0c70eeea4efb52b0a5e2e51d7499fb93eb16e5
Girish <kumargirish2301@gmail.com>
2024-07-22 23:11:10 +0530

    added learn.js

---
+++ Js/learn.js
@@ -0,0 +1,7 @@
+//Programming- instructions to tell computer to what to do.
+
+//Ecmascript- Js standard to make different documents recognize that they are using same language
+
+//Ways to execute Js- browser console,node runtime,html script tag,replit
+
+//Variables- to store values with different datatypes
