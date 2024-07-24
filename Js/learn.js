325ed9dce7b1198cb1c83e99310985602d3f18ae
Girish <kumargirish2301@gmail.com>
2024-07-24 23:58:10 +0530

    updated learn.js

--- Js/learn.js
+++ Js/learn.js
@@ -5,3 +5,52 @@
 //Ways to execute Js- browser console,node runtime,html script tag,replit
 
 //Variables- to store values with different datatypes
+
+//Dynamic typed language-variables can change type on runtime
+
+// let a=53;
+// console.log(a);
+// a="girish";
+// console.log(a);
+
+//Js is case-sensitive and don't create variables from reserved keywords
+
+    /* OUTPUT */
+/***************************
+
+53
+girish
+
+***************************/
+
+// var-globally scoped,redeclaration,default is undefined
+// let-local scoped,no redeclaration
+//const-local scoped,no redeclearation and redefinition
+
+// let b=23;
+// let b="girish"; //throws error bcoz of reclaration
+
+// Primitive Datatypes-null,number,bool,bigint,String,Symbol,Undefined
+
+// let a=null;
+// let b=234;
+// let c=false;
+// let d=BigInt("23553");
+// let e="Girish";
+// let f=Symbol("I am Symbol");
+// let g=undefined;
+
+// console.log(typeof d); //bigint //to know type of variable
+
+
+//Object is Non-Primitive DataType and acts as key-value pair
+
+// const item={"Girish":23,"hisoka":"hi"};
+// console.log(item);
+
+    /* OUTPUT */
+/***************************
+
+{Girish: 23, hisoka: 'hi'}
+
+***************************/
